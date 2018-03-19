df77cd9a4c22d2d5537e2da032b07cad9330374b
mobeen7asif <mobeen.asif@codingpixel.com>
2018-03-19 17:54:05 +0500

    commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,17 +4,31 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("//news/{article}")
+     * @Route("/news/{article}")
      */
     public function homepage($article)
     {
         return new Response("My first new page of s".$article);
+    }
+
+    /**
+     * @Route("/show/{slug}")
+     */
+    public function showArticles($slug){
+        //return new Response("My first new page of s".$slug);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig',['comments' => $comments]);
     }
 }
